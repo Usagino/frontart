cd0c2e5cf9df2cbea362582fa31c2296ee372cef
Usagino <yuki.itoh@front-artgraph.com>
2019-05-08 10:15:51 +0900

    upload

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  section.container
+   h1 toppage
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style lang="scss">
+h1{
+  color: red;
+}
+</style>
