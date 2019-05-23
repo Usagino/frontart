2a5c9a3012819b809aed0b175555af678f8b31d8
Usagino <yuki.itoh@front-artgraph.com>
2019-05-23 18:15:10 +0900

    uploda

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,27 @@
 <template lang="pug">
   .container
     cursorPointer
+    sideMenu
     section
-      lottie.first-view__wrap--image(
-        :options="lottieOptions"
-        v-on:animCreated="handleAnimation")
+      .first
+        .first__wrap
+          .first__wrap__logo
+            lottie.first-view__wrap--image(
+              :options="lottieOptions"
+              v-on:animCreated="handleAnimation")
 </template>
 
 <script>
 import * as animationData from "~/assets/json/my_logo.json";
 import Lottie from "~/components/Lottie.vue";
+import sideMenu from "~/components/sideMenu.vue";
 import cursorPointer from '@/components/cursorPointer'
 
 export default {
   components: {
     Lottie,
-    cursorPointer
+    cursorPointer,
+    sideMenu
   },
   data(){
     return {
@@ -32,10 +38,17 @@ export default {
 
 <style lang="scss" scoped>
 section{
-  height: 100vh;
-  width: 100vw;
+  .first{
+    &__wrap{
+      height: 100vh;
+      width: 100vw;
+      @include middle;
+      &__logo{
+        width: 40vw;
+        height: auto;
+      }
+    }
+  }
 }
-h1{
-  color: red;
-}
+
 </style>
